639e5d19cf2c032b2207fcee09bfce2b16d2791e
Michail Pompushko <MPompushko@gmail.com>
2023-12-02 18:12:44 +0300

    start

---
+++ MFCLibrary1.cpp
@@ -0,0 +1,64 @@
+﻿// MFCLibrary1.cpp: определяет процедуры инициализации для библиотеки DLL.
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "MFCLibrary1.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+//
+//TODO: если эта библиотека DLL динамически связана с библиотеками DLL MFC,
+//		все функции, экспортированные из данной DLL-библиотеки, которые выполняют вызовы к
+//		MFC, должны содержать макрос AFX_MANAGE_STATE в
+//		самое начало функции.
+//
+//		Например:
+//
+//		extern "C" BOOL PASCAL EXPORT ExportedFunction()
+//		{
+//			AFX_MANAGE_STATE(AfxGetStaticModuleState());
+//			// тело нормальной функции
+//		}
+//
+//		Важно, чтобы данный макрос был представлен в каждой
+//		функции до вызова MFC.  Это означает, что
+//		должен стоять в качестве первого оператора в
+//		функции и предшествовать даже любым объявлениям переменных объекта,
+//		поскольку их конструкторы могут выполнять вызовы к MFC
+//		DLL.
+//
+//		В Технических указаниях MFC 33 и 58 содержатся более
+//		подробные сведения.
+//
+
+// CMFCLibrary1App
+
+BEGIN_MESSAGE_MAP(CMFCLibrary1App, CWinApp)
+END_MESSAGE_MAP()
+
+
+// Создание CMFCLibrary1App
+
+CMFCLibrary1App::CMFCLibrary1App()
+{
+	// TODO: добавьте код создания,
+	// Размещает весь важный код инициализации в InitInstance
+}
+
+
+// Единственный объект CMFCLibrary1App
+
+CMFCLibrary1App theApp;
+
+
+// Инициализация CMFCLibrary1App
+
+BOOL CMFCLibrary1App::InitInstance()
+{
+	CWinApp::InitInstance();
+
+	return TRUE;
+}
---
+++ MFCLibrary1.h
@@ -0,0 +1,27 @@
+﻿// MFCLibrary1.h: основной файл заголовка для библиотеки DLL MFCLibrary1
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "включить pch.h до включения этого файла в PCH"
+#endif
+
+#include "resource.h"		// основные символы
+
+
+// CMFCLibrary1App
+// Реализацию этого класса см. в файле MFCLibrary1.cpp
+//
+
+class CMFCLibrary1App : public CWinApp
+{
+public:
+	CMFCLibrary1App();
+
+// Переопределение
+public:
+	virtual BOOL InitInstance();
+
+	DECLARE_MESSAGE_MAP()
+};
